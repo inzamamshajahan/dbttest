badadb299247e131de3b39e6796724732c283c26
inzamaminbox@gmail.com <inzamaminbox@gmail.com>
2023-09-20 13:04:49 +0000

    added generic tests like unique, not null

--- models/staging/jaffle_shop/stg_customer.sql
+++ models/staging/jaffle_shop/stg_customer.sql
@@ -5,7 +5,7 @@ with customers as (
         first_name,
         last_name
 
-    from {{source('jaffle_shop','customer')}}
+    from {{source('jaffle_shop','customers')}}
 
 )
 select * from customers
